db23d33f717bcb03c4b1d24645122a41b67ec679
vic778 <victoiremmanuelbarh@gmail.com>
2022-06-26 18:37:13 +0300

    Permit the body

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username profile_picture])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username profile_picture phone])
   end
 end
