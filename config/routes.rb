3120ea9112ca3d602fb50a9fd3283127f76d2bc6
vic778 <victoiremmanuelbarh@gmail.com>
2022-06-26 18:38:55 +0300

    Create auto log

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
    scope :api, defaults: { format: :json } do
       devise_for :users, controllers: { sessions: :sessions }, path_names: { sign_in: :login }
 
+      resource :user, only: [:update]
+      get 'user/auto_login', to: 'users#auto_login'
 
+      # resources :users
+      
 
    end
 end
