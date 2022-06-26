b958d0aff784f8bd4f86f5db46fc5fe041130dc8
vic778 <victoiremmanuelbarh@gmail.com>
2022-06-26 17:15:05 +0300

    Fix linters errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,20 +7,16 @@ class User < ApplicationRecord
   belongs_to :role
   after_initialize :set_default_role
 
-
-
   def update_role(role_name)
     self.role = Role.find_or_create_by(name: role_name)
-    self.save
+    save
   end
 
- 
   def generate_jwt
     JWT.encode({ id: id, exp: 2.days.from_now.to_i }, 'vicSecret')
   end
 
-  
-  private 
+  private
 
   def set_default_role
     self.role ||= Role.find_by(name: 'user') if role.nil?
