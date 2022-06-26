c94f63eca062d23e9fe66b3bcc40ddbde9fac469
vic778 <victoiremmanuelbarh@gmail.com>
2022-06-26 18:39:36 +0300

    Migrate phone number to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_26_134547) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_26_152913) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,6 +67,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_26_134547) do
     t.string "username"
     t.string "profile_picture"
     t.bigint "role_id"
+    t.integer "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["role_id"], name: "index_users_on_role_id"
