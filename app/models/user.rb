595411a6deb8571c83da19bc106e4a3fc59a1a33
vic778 <victoiremmanuelbarh@gmail.com>
2022-06-26 18:37:50 +0300

    Initialize the role by defult

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable, :trackable
+         :recoverable, :rememberable, :validatable, :trackable
 
   belongs_to :role
   after_initialize :set_default_role
