4ef45b3234619378af1ac893e5cba003c173486b
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-08 17:27:47 +0300

    Associate with user and class_room

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,7 @@
 class Room < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
-  validates :field, presence: true, uniqueness: true
+    has_many :class_rooms
+    has_many :users, through: :class_rooms
+
+    validates :name, presence: true, uniqueness: true
+    validates :field, presence: true, uniqueness: true
 end
