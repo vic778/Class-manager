4401b9d70b250e85923ad18928356d0fcd09bd02
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-11 11:47:12 +0300

    Associate with homw_work

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,7 @@
 class Room < ApplicationRecord
   has_many :class_rooms
   has_many :users, through: :class_rooms
+  has_many :home_works
 
   validates :name, presence: true, uniqueness: true
   validates :field, presence: true, uniqueness: true
