c337b5a3219e0874873d3712b1f5d86f648bf920
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-11 19:42:14 +0300

    Fix linter errors

--- app/controllers/student_ups_controller.rb
+++ app/controllers/student_ups_controller.rb
@@ -1,8 +1,7 @@
-class StudentUpsController < PermissionsController 
+class StudentUpsController < PermissionsController
   before_action :authenticate_user!
   before_action :only_student
-  before_action :set_student_up, only: [:show, :update, :destroy]
-
+  before_action :set_student_up, only: %i[show update destroy]
 
   def index
     @room = Room.find(params[:room_id])
@@ -13,47 +12,46 @@ class StudentUpsController < PermissionsController
     # render json: @student_ups
   end
 
-    def show
-        @student_up = helpers.student_name(params[:id])
-        render json: @student_up
+  def show
+    @student_up = helpers.student_name(params[:id])
+    render json: @student_up
+  end
+
+  def create
+    @student_up = StudentUp.create(student_up_params.merge(room_id: params[:room_id], user_id: current_user.id, home_work_id: params[:home_work_id]))
+    if @student_up.save
+      render json: { status: true, message: "StudentUp created successfully", student_up: @student_up }, status: :created
+    else
+      render json: @student_up.errors, status: :unprocessable_entity
     end
+  end
 
-    def create
-        @student_up = StudentUp.create(student_up_params.merge(room_id: params[:room_id], user_id: current_user.id, home_work_id: params[:home_work_id]))
-        if @student_up.save
-            render json: { status: true, message: "StudentUp created successfully", student_up: @student_up }, status: :created
-        else
-            render json: @student_up.errors, status: :unprocessable_entity
-        end
+  # def update
+  #     if @student_up.update(student_up_params)
+  #         render json: { success: true, message: "StudentUp updated successfully", student_up: @student_up }
+  #     else
+  #         render json: @student_up.errors, status: :unprocessable_entity
+  #     end
+  # end
+
+  def destroy
+    if @student_up.destroy
+      render json: { success: true, message: "StudentUp deleted successfully" }
+    else
+      render json: @student_up.errors, status: :unprocessable_entity
     end
+  end
 
-    # def update
-    #     if @student_up.update(student_up_params)
-    #         render json: { success: true, message: "StudentUp updated successfully", student_up: @student_up }
-    #     else
-    #         render json: @student_up.errors, status: :unprocessable_entity
-    #     end
-    # end
+  private
 
-    def destroy
-        if @student_up.destroy
-            render json: { success: true, message: "StudentUp deleted successfully" }
-        else
-            render json: @student_up.errors, status: :unprocessable_entity
-        end
-    end
+  def set_student_up
+    @room = Room.find(params[:room_id])
+    @home_work = HomeWork.find(params[:home_work_id])
+    @student_up = @home_work.student_ups.find(params[:id])
+    # @student_up = @room.student_ups.find(params[:id])
+  end
 
-    private
-
-    def set_student_up
-        @room = Room.find(params[:room_id])
-        @home_work = HomeWork.find(params[:home_work_id])
-        @student_up = @home_work.student_ups.find(params[:id])
-        # @student_up = @room.student_ups.find(params[:id])
-    end
-
-    def student_up_params
-        params.permit(:assignment)
-    end
-
+  def student_up_params
+    params.permit(:assignment)
+  end
 end
--- app/helpers/student_ups_helper.rb
+++ app/helpers/student_ups_helper.rb
@@ -1,12 +1,11 @@
 module StudentUpsHelper
+  def student_name(id)
+    @room = Room.find(params[:room_id])
+    @home_work = @room.home_works.find(params[:id])
+    @student_up = @home_work.student_ups.find(id)
+    @student = User.find(@student_up.user_id)
 
-    def student_name(id)
-        @room = Room.find(params[:room_id])
-        @home_work = @room.home_works.find(params[:id])
-        @student_up = @home_work.student_ups.find(id)
-        @student = User.find(@student_up.user_id)
-
-        # @student_up = StudentUp.find(id)
-        # @student_up.user.username
-    end
+    # @student_up = StudentUp.find(id)
+    # @student_up.user.username
+  end
 end
