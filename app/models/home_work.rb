5dd997858458fcb9c40f50f07e75dd140a1da329
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-11 19:35:12 +0300

    Add student_up_id to home_work admin panel

--- app/models/home_work.rb
+++ app/models/home_work.rb
@@ -1,6 +1,7 @@
 class HomeWork < ApplicationRecord
   belongs_to :room
   belongs_to :user
+  has_many :student_ups
 
   validates :assignment, presence: true
   validates :course_name, presence: true
