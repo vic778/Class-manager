61e106a183c0de366ea645ecb9415c29b919c64d
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-14 22:47:56 +0300

    Sign in a user as an admin before doing any action

--- spec/requests/rooms_spec.rb
+++ spec/requests/rooms_spec.rb
@@ -1,7 +1,47 @@
 require 'rails_helper'
+require '././spec/support/login_helpers.rb'
 
 RSpec.describe "Rooms", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  context "when as an amdin" do
+    let!(:user) { create(:user, :admin) }
+    let(:room) { create(:room) }
+    # before { login_user user }
+
+    describe "GET /rooms" do
+      it "returns a list of rooms" do
+        get rooms_path
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe "GET /rooms/:id" do
+      it "returns a room" do
+        get room_path(room)
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe "POST /rooms" do
+      it "creates a room" do
+        post rooms_path, params: { room: { name: "New Room" } }
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe "PUT /rooms/:id" do
+      it "updates a room" do
+        put room_path(room), params: { room: { name: "New Room" } }
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe "DELETE /rooms/:id" do
+      it "deletes a room" do
+        delete room_path(room)
+        expect(response).to have_http_status(200)
+      end
+    end
+    
   end
+  
 end
