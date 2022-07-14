f5f98f43e34fd7b840e86827918b689b06397fa1
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-14 16:56:40 +0300

    Assciete to role

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "it validates factory" do
+    expect(build(:user)).to be_valid
+  end
+
+  it { should belong_to(:role) }
+  it {is_expected.to validate_presence_of(:username)}
+  it {is_expected.to validate_presence_of(:email)}
+  it {is_expected.to validate_presence_of(:password)}
+  it {is_expected.to validate_presence_of(:password_confirmation)}
 end
