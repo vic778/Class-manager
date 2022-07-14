e6640a2a7b2e7bbb62c7b7eb35e1aeb027efae35
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-14 16:52:53 +0300

    Associated the room and user to class-room

--- spec/models/class_room_spec.rb
+++ spec/models/class_room_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe ClassRoom, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "it validates factory" do
+    expect(build(:class_room)).to be_valid
+  end
+
+  it {should belong_to(:user)}
+  it {should belong_to(:room)}
+ 
 end
