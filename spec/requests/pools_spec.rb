65dc03a7e580876d1944f7053f56ddc37f1bf7c3
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-15 11:50:47 +0300

    Logs a user and take actions

--- spec/requests/pools_spec.rb
+++ spec/requests/pools_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Pools", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  context "when as an admin" do
+   let!(:user) { create(:user, :admin) }
+  
+    describe "GET /pools" do
+      before { get pools_path }
+      it "returns a list of students in a pools where status is true" do
+        expect(response).to have_http_status(200)
+      end
+    end  
   end
+  
 end
