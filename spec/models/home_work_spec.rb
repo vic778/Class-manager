6b85a78be2278c7d37ab54f251b3307577c0d004
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-15 17:26:26 +0300

    CHeck if room_id is present and if there's user_id

--- spec/models/home_work_spec.rb
+++ spec/models/home_work_spec.rb
@@ -9,4 +9,25 @@ RSpec.describe HomeWork, type: :model do
   it { should belong_to(:room) }
   it { is_expected.to validate_presence_of(:assignment) }
   it { is_expected.to validate_presence_of(:course_name) }
+
+  describe "when user_id is not present" do
+    before { @home_work = HomeWork.new(user_id: "") }
+    it { expect(@home_work).not_to be_valid }
+  end
+
+  describe "when room_id is not present" do
+    before { @home_work = HomeWork.new(room_id: "") }
+    it { expect(@home_work).not_to be_valid }
+  end
+
+  describe "when assignment is not present" do
+    before { @home_work = HomeWork.new(assignment: "") }
+    it { expect(@home_work).not_to be_valid }
+  end
+
+  describe "when course_name is not present" do
+    before { @home_work = HomeWork.new(course_name: "") }
+    it { expect(@home_work).not_to be_valid }
+  end
+
 end
