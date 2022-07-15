a07c883daab4cec04d86d18f50e884b00e0c8609
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-15 17:27:43 +0300

    Check if the filed column is present

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -7,4 +7,26 @@ RSpec.describe Room, type: :model do
 
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_presence_of(:field) }
+
+  describe "when name is not present" do
+    before { @room = Room.new(name: "") }
+    it { expect(@room).not_to be_valid }
+  end
+
+  describe "when field is not present" do
+    before { @room = Room.new(field: "") }
+    it { expect(@room).not_to be_valid }
+  end
+
+  describe "when name is unique" do
+    before { @room = Room.create(name: "room1", field: "ww") }
+    it { expect(@room).to be_valid }
+  end
+
+  describe "when name is not unique" do
+    before { @room = Room.create(name: "room1", field: "ww") }
+    before { @room2 = Room.new(name: "room1", field: "ww") }
+    it { expect(@room2).not_to be_valid }
+  end
+  
 end
