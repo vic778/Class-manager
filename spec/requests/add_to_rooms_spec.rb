28e5e1947962752e5af27b3340fb061e54360a2d
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-15 10:37:06 +0300

    Change the path to add a room

--- spec/requests/add_to_rooms_spec.rb
+++ spec/requests/add_to_rooms_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "AddToRooms", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let!(:user) { create(:user) }
+  let(:room) { create(:room) }
+
+  describe "POST /rooms/:id/users" do
+    it "adds a user to a room" do
+      post add_to_rooms_path, params: { user: { username: "new_username" } }
+      expect(response).to have_http_status(200)
+    end
   end
 end
