c3e6c2b7faf8ee2dae598831b53b9c6acf27a67a
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-15 17:28:15 +0300

    Describe present of home_work _id

--- spec/models/student_up_spec.rb
+++ spec/models/student_up_spec.rb
@@ -9,4 +9,25 @@ RSpec.describe StudentUp, type: :model do
   it { should belong_to(:room) }
   it { should belong_to(:home_work) }
   it { is_expected.to validate_presence_of(:assignment) }
+
+  describe "when user_id is not present" do
+    before { @student_up = StudentUp.new(user_id: "") }
+    it { expect(@student_up).not_to be_valid }
+  end
+
+  describe "when room_id is not present" do
+    before { @student_up = StudentUp.new(room_id: "") }
+    it { expect(@student_up).not_to be_valid }
+  end
+
+  describe "when home_work_id is not present" do
+    before { @student_up = StudentUp.new(home_work_id: "") }
+    it { expect(@student_up).not_to be_valid }
+  end
+
+  describe "when assignment is not present" do
+    before { @student_up = StudentUp.new(assignment: "") }
+    it { expect(@student_up).not_to be_valid }
+  end
+
 end
